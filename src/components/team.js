11eb65a9ff68b88505c73daac39a0fab719fd01c
Bogdan Chelariu <bogdan.chelariu@azets.com>
2018-09-27 14:09:07 +0300

    GameOn - redesign

--- src/components/team.js
+++ src/components/team.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import logo from '../img/players.png';
 import cup from '../img/cup.png';
 
 import Player from './player';
@@ -16,10 +15,11 @@ class Team extends Component {
 
     return (
       <div className="team">
-        <img src={logo} alt="playerLogo" />
         <Player name={this.props.player1.Nickname} mmr={this.props.player1.MMR} setPlayer={()=>this.props.setPlayer(this.props.teamNo*2 + 1)} />
         <Player name={this.props.player2.Nickname} mmr={this.props.player2.MMR} setPlayer={()=>this.props.setPlayer(this.props.teamNo*2 + 2)}/>
-        <img src={cup} alt="cupLogo" onClick={this.props.setAsWinner} className={cupClass.join(' ')} />
+		<div>
+        	<img src={cup} alt="cupLogo" onClick={this.props.setAsWinner} className={cupClass.join(' ')} />
+		</div>
       </div>
     );
   }
